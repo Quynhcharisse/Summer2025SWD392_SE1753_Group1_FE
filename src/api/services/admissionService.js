8159179ca6bfd1a339e4990a3bd707422f288f58
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-06-11 15:45:21 +0700

    fix: fix update term

--- src/api/services/admissionService.js
+++ src/api/services/admissionService.js
@@ -1,48 +1,82 @@
+
 import apiClient from "@api/apiClient.js";
 
 export const getTermList = async () => {
-  try {
-    const response = await apiClient.get("/admission/term");
-
-    return response.data;
-  } catch (error) {
-    console.error("Get term list error:", error);
-    throw error;
-  }
-};
+    try {
+        const response = await
+           apiClient.get("/admission/term")
+        return response.data
+    } catch (error) {
+        console.error("Get term list error:", error);
+        throw error;
+    }
+}
 
 export const createTerm = async (
-  grade,
-  startDate,
-  endDate,
-  maxNumberRegistration,
-  reservationFee,
-  serviceFee,
-  uniformFee,
-  learningMaterialFee,
-  facilityFee
+    grade,
+    startDate,
+    endDate,
+    maxNumberRegistration,
+    reservationFee,
+    serviceFee,
+    uniformFee,
+    learningMaterialFee,
+    facilityFee
 ) => {
-  console.log("Grade: ", grade);
 
-  try {
-    const response = await apiClient.post("/admission/term", {
-      grade: grade,
-      startDate: startDate,
-      endDate: endDate,
-      maxNumberRegistration: maxNumberRegistration,
-      reservationFee: reservationFee,
-      serviceFee: serviceFee,
-      uniformFee: uniformFee,
-      learningMaterialFee: learningMaterialFee,
-      facilityFee: facilityFee,
-    });
+    try {
+        const response = await
+            apiClient.post("/admission/term", {
+                grade: grade,
+                startDate: startDate,
+                endDate: endDate,
+                maxNumberRegistration: maxNumberRegistration,
+                reservationFee: reservationFee,
+                serviceFee: serviceFee,
+                uniformFee: uniformFee,
+                learningMaterialFee: learningMaterialFee,
+                facilityFee: facilityFee
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Create term error:", error);
+        throw error;
+    }
+}
 
-    console.log(response);
 
-    return response.data;
-  } catch (error) {
-    console.log(response);
-    console.error("Create term error:", error);
-    throw error;
-  }
-};
+export const updateTerm = async (
+    id,
+    grade,
+    startDate,
+    endDate,
+    maxNumberRegistration,
+    reservationFee,
+    serviceFee,
+    uniformFee,
+    learningMaterialFee,
+    facilityFee
+) => {
+
+    try {
+        const response = await
+            apiClient.put("/admission/term", {
+                id: id,
+                grade: grade,
+                startDate: startDate,
+                endDate: endDate,
+                maxNumberRegistration: maxNumberRegistration,
+                reservationFee: reservationFee,
+                serviceFee: serviceFee,
+                uniformFee: uniformFee,
+                learningMaterialFee: learningMaterialFee,
+                facilityFee: facilityFee
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Update term error:", error);
+        throw error;
+    }
+}
